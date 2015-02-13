2886d7fada381248c8acfde37db43d66d4a7803e
dimka-sark-test <dimkasark@gmail.com>
2015-02-13 14:24:16 +0300

    trevis try 8

--- app/src/androidTest/java/test/opengles/ApplicationTest.java
+++ app/src/androidTest/java/test/opengles/ApplicationTest.java
@@ -10,4 +10,15 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        createApplication();
+    }
+
+    public void testMegaTest() throws Exception {
+        assertTrue("Zdarova", true);
+        assertFalse("Zdarova 2", true);
+    }
 }
